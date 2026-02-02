8a93ee05adc98fd7d797d4995de9f18b154d79ba
amandaP-93 <amandapatricia0610@gmail.com>
2026-02-03 00:31:33 +0700

    Add datatypes.py with basic data type examples

---
+++ Amanda-Patricia_25071206390_TIC/pertemuan-1/latihan-praktikum/datatypes.py
@@ -0,0 +1,12 @@
+# datatypes.py
+# Contoh tipe data dasar di Python
+
+angka_int = 10        # integer
+angka_float = 3.14   # float
+teks = "Python"      # string
+status = True        # boolean
+
+print(type(angka_int))
+print(type(angka_float))
+print(type(teks))
+print(type(status))
