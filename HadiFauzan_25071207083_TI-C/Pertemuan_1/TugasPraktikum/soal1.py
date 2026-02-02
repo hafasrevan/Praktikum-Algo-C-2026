f013427888a07351ba46770e3793f737330308a6
diikd12 <hadifauzan255@gmmail.com>
2026-02-02 10:04:11 +0700

    adding TugasPraktikum

---
+++ HadiFauzan_25071207083_TI-C/Pertemuan_1/TugasPraktikum/soal1.py
@@ -0,0 +1,11 @@
+#Masukkan input dari user
+
+nilai1 = 10
+nilai2 = 20
+
+#output bentuk menampilkan nilai
+print ('Nilai 1 =', nilai1)
+print ('Nilai 2 =', nilai2)
+
+#atau mau output hasil penjumlahan
+print ('Hasil perjumlahan dari nilai 1 dan nilai 2 adalah =', nilai1 + nilai2)
