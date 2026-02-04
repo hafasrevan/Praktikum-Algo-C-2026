301450de9777bb6e3c2c4fdad7256ca328813881
hafasrevan <hafastreifan@gmail.com>
2026-02-04 21:48:09 +0700

    update

---
+++ Hafas_25071206984_TI-C/Pertemuan-1/tugas-praktikum/soal2.py
@@ -0,0 +1,11 @@
+x = int (input("masukkan nilai {x}"))
+y = int (input("masukkan nilai {y}"))
+
+z = x + y
+z2 = x * y 
+
+print()
+print (f"Hasil dari {x} ditambah {y} adalah {z}")
+print ()
+print (f"Hasil dari {x} dikali {y} adalah {z2}")
+print()
---
+++ Hafas_25071206984_TI-C/Pertemuan-1/tugas-praktikum/soal5.py
@@ -0,0 +1,12 @@
+def Koenci_Function():
+    print (f"the result of the plus is : {z}")
+    print ()
+    print (f"the result of the multiply is : {z2}")
+    print ()
+
+x = int(input("Masukkan nilai pertama : "))
+y = int(input("Masukkan nilai kedua :"))
+z = x + y
+z2 = x * y
+
+Koenci_Function()
